0c69fcd288874d104f49c249922d24a5c1014392
aonych <aleksandrao995@gmail.com>
2017-01-05 22:36:09 +0100

    Pobierz wszystkie filmy na podstawie kraju - test

--- src/test/java/com/example/shdemo/service/MovieManagerTest.java
+++ src/test/java/com/example/shdemo/service/MovieManagerTest.java
@@ -30,15 +30,20 @@ public class MovieManagerTest {
 	private final String PRODUCTION_1 = "Walt Disney Pictures"; 
 	private final int YEAR_1 = 2013;
 	
-	private final String TITLE_2 = "Madagascar";
-	private final String COUNTRY_2 = "USA"; 
-	private final String PRODUCTION_2 = "DreamWorks"; 
-	private final int YEAR_2 = 2005;
+	private final String TITLE_2 = "Asterix i Kleopatra";
+	private final String COUNTRY_2 = "Francja"; 
+	private final String PRODUCTION_2 = "Raymond Leblanc"; 
+	private final int YEAR_2 = 1968;
 	
-	private final String TITLE_3 = "Cars";
-	private final String COUNTRY_3 = "USA"; 
-	private final String PRODUCTION_3 = "Pixar Animation Studios"; 
+	private final String TITLE_3 = "Franklin i skarb jeziora";
+	private final String COUNTRY_3 = "Francja"; 
+	private final String PRODUCTION_3 = "Mars Distribution"; 
 	private final int YEAR_3 = 2006;
+	
+	private final String TITLE_4 = "Madagascar";
+	private final String COUNTRY_4 = "USA"; 
+	private final String PRODUCTION_4 = "DreamWorks"; 
+	private final int YEAR_4 = 2005;
 	
 	@Test
 	public void checkAdd(){
@@ -135,5 +140,38 @@ public class MovieManagerTest {
 		assertEquals(YEAR_2, movieRetrieved.getYear());
 		
 	}
+	
+	@Test
+	public void checkGetByCountry(){
+		Movie movie1 = new Movie(TITLE_1, COUNTRY_1,PRODUCTION_1,YEAR_1);
+		Movie movie2 = new Movie(TITLE_2, COUNTRY_2,PRODUCTION_2,YEAR_2);
+		Movie movie3 = new Movie(TITLE_3, COUNTRY_3,PRODUCTION_3,YEAR_3);
+		Movie movie4 = new Movie(TITLE_4, COUNTRY_4,PRODUCTION_4,YEAR_4);
+		
+		moviesManager.addMovie(movie1);
+		moviesManager.addMovie(movie2);
+		moviesManager.addMovie(movie3);
+		moviesManager.addMovie(movie4);
+		
+		List<Movie> movies = moviesManager.getMovieByCountry(COUNTRY_2);
+		int count = moviesManager.getMovieByCountry(COUNTRY_2).size();
+		
+		Movie movieRetrieved;
+		assertEquals(count,2);
+		
+		movieRetrieved = moviesManager.getMovie(movies.get(0).getId());
+		assertEquals(TITLE_2, movieRetrieved.getTitle());
+		assertEquals(COUNTRY_2, movieRetrieved.getCountry());
+		assertEquals(PRODUCTION_2, movieRetrieved.getProduction());
+		assertEquals(YEAR_2, movieRetrieved.getYear());
+		
+		movieRetrieved = moviesManager.getMovie(movies.get(1).getId());
+		assertEquals(TITLE_3, movieRetrieved.getTitle());
+		assertEquals(COUNTRY_3, movieRetrieved.getCountry());
+		assertEquals(PRODUCTION_3, movieRetrieved.getProduction());
+		assertEquals(YEAR_3, movieRetrieved.getYear());
+	}
+	
+	
 	
 }
